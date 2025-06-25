e0a2afeb617a7731ed14fef316876ec579558d44
Lancerhb2 <61063992+Lancerhb2@users.noreply.github.com>
2025-06-25 12:42:33 -0400

    Added bot token

--- 3Bot.py
+++ 3Bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 
+BOT_TOKEN = "1387463987712626910"
 intents = discord.Intents.default()
 intents.message_content = True
 
@@ -29,4 +30,4 @@ async def on_message(message):
 
     await bot.process_commands(message)
 
-bot.run("YOUR_BOT_TOKEN")
+bot.run("BOT_TOKEN")
